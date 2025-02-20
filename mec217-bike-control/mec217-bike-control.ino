afc91deed95032d9a6a2c60442c9129adcf93584
Dan Brennan <d.s.brennan@sheffield.ac.uk>
2025-02-20 16:13:17 +0000

    Remove timing logic and improve system responsiveness
    Remove the timing logic as it is no longer required within this sketch.
    Improve the responsiveness of the system by reducing the delay
    within the main loop, adjusting the step up and down values, and
    adjusting the crank and wheel circumferences. Additionally, bring
    the code inline with the instrument cluster style.

--- mec217-bike-control/mec217-bike-control.ino
+++ mec217-bike-control/mec217-bike-control.ino
@@ -1,149 +1,178 @@
 /**
  *  Original Author: D S Brennan (github.com/dsbrennan)
  *  Created: 29/08/2023
+ *  Updated: 20/02/2025
  *
- *  Copyright 2023, MIT Licence
+ *  Copyright 2023 - 2025, MIT Licence
  **/
 #include <Servo.h>
-//Pin setup
-const int SYSTEM_READY_LED_PIN = 5;
-const int MOTOR_ACTIVITY_LED_PIN = 6;
-const int CRANK_ACTIVITY_LED_PIN = 4;
-const int CRANK_PIN = 2;
-const int RPM_PIN = 3;
-const int ESC_PIN = 9;
-//Constants
-const int startup_time = 5000;
-const int crank_pass_maximum_delay = 1500;
-const int wheel_rotation_time = 30000;
+
+// pins: do not change these values
+#define SYSTEM_READY_LED_PIN 5
+#define CRANK_ACTIVITY_LED_PIN 4
+#define MOTOR_ACTIVITY_LED_PIN 6
+#define CRANK_PIN 2
+#define WHEEL_PIN 3
+#define ESC_PIN 9
+
+// safety: do not change these values
+#define CRANK_PASS_ACTIVITY_DELAY 500
+#define CRANK_PASS_MAXIMUM_DELAY 3000
+#define WHEEL_MAXIMUM_SPEED 25.0
+
+// bike: crank radius 0.175m, wheel radius 0.33m
+#define CRANK_CIRCUMFORANCE 1.0995
+#define WHEEL_CIRCUMFORANCE 2.0734
+
+// debug
+#define MESSAGE_MAXIMUM_INTERVAL 1000
+
+// constants, can be tweaked
+const int startup_time = 3000;
 const int esc_initial_power = 85;
-const int esc_power_limit = 140;
-const int esc_step_up = 1;
-const int esc_step_down = 5;
-const float wheel_circumforance = 2.0;
-const float wheel_maximum_speed = 25.0;
-//Variables
-unsigned volatile long last_crank_pass;
-unsigned volatile long last_rpm_pass;
-unsigned volatile long last_rpm_pass_2;
-unsigned volatile int wheel_rotations_count;
+const int esc_power_limit = 180;
+const float esc_step_up = 0.1;
+const float esc_step_down = 0.5;
+
+// count variables
+unsigned volatile int crank_rotations_counter;
+unsigned volatile int wheel_rotations_counter;
+unsigned volatile long crank_interrupt_current_time;
+unsigned volatile long crank_interrupt_previous_time;
+unsigned volatile long wheel_interrupt_current_time;
+unsigned volatile long wheel_interrupt_previous_time;
 unsigned long current_loop_time;
-unsigned long esc_activation_time;
-signed int timer_activation_count;
-signed int timer_deactivation_count;
-unsigned int esc_power_output;
+unsigned long message_previous_time;
+
+// output variables
+float esc_power_output;
 Servo esc;
 
-//Setup System
+/*
+  System Setup
+  ---------------------
+*/
 void setup() {
+  // serial
   Serial.begin(9600);
-  //Setup Status LEDs
+  // set default values
+  crank_rotations_counter = 0;
+  wheel_rotations_counter = 0;
+  crank_interrupt_current_time = 0;
+  crank_interrupt_previous_time = 0;
+  wheel_interrupt_current_time = 0;
+  wheel_interrupt_previous_time = 0;
+  current_loop_time = 0;
+  message_previous_time = 0;
+  // status LEDs
   pinMode(SYSTEM_READY_LED_PIN, OUTPUT);
   digitalWrite(SYSTEM_READY_LED_PIN, LOW);
   pinMode(CRANK_ACTIVITY_LED_PIN, OUTPUT);
   digitalWrite(CRANK_ACTIVITY_LED_PIN, LOW);
   pinMode(MOTOR_ACTIVITY_LED_PIN, OUTPUT);
   digitalWrite(MOTOR_ACTIVITY_LED_PIN, LOW);
-  //Setup Crank Sensor
+  // crank sensor
   pinMode(CRANK_PIN, INPUT);
-  attachInterrupt(digitalPinToInterrupt(CRANK_PIN), crank, FALLING);
-  //Setup RPM Sensor
-  pinMode(RPM_PIN, INPUT);
-  attachInterrupt(digitalPinToInterrupt(RPM_PIN), rpm, FALLING);
-  //Setup Timer
-  timer_activation_count = -1;
-  timer_deactivation_count = -1;
-  //Setup ESC
+  attachInterrupt(digitalPinToInterrupt(CRANK_PIN), crankInterrupt, FALLING);
+  // wheel sensor
+  pinMode(WHEEL_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(WHEEL_PIN), wheelInterrupt, FALLING);
+  // ESC
   esc_power_output = esc_initial_power;
   esc.attach(ESC_PIN);
   esc.write(0);
-  //Pause System untill ready
+  // pause system untill ready
   long zero_time = millis();
-  while (millis() < zero_time + startup_time){
+  while (millis() < zero_time + startup_time) {
     delay(1000);
   }
-  //Activate System ready LED
+  // activate system ready LED
   digitalWrite(SYSTEM_READY_LED_PIN, HIGH);
   Serial.println("System Ready");
 }
 
-//Main function loop
+/*
+  Main Loop
+  ---------------------
+*/
 void loop() {
-  //Show Crank LED
-  digitalWrite(CRANK_ACTIVITY_LED_PIN, ((digitalRead(CRANK_PIN) == 1) ? LOW : HIGH));
-  //Calculate RPM
-  float rpm_pass_delta = last_rpm_pass - last_rpm_pass_2;
-  float rpm_value = (float)60000.0 / rpm_pass_delta;
-  float rph_value = rpm_value * 60;
-  float rph_meter = rph_value * wheel_circumforance;
-  float kmph = rph_meter / 1000;
-  // Serial.print("Wheel spinning at: ");
-  // Serial.println(kmph);
-  //Last crank pass within time limit
+  // show crank activity LED
+  digitalWrite(CRANK_ACTIVITY_LED_PIN, (current_loop_time - crank_interrupt_current_time <= CRANK_PASS_ACTIVITY_DELAY ? HIGH : LOW));
+
+  // calculate crank speed
+  float crank_kmph = 0;
+  float crank_rotation_time = crank_interrupt_current_time - crank_interrupt_previous_time;
+  if (crank_rotation_time > 1) {
+    float crank_rpm = 60.0 / (crank_rotation_time / 1000);
+    crank_kmph = (crank_rpm * 60 * CRANK_CIRCUMFORANCE) / 1000;
+  }
+
+  // calculate wheel speed
+  float wheel_kmph = 0;
+  float wheel_rotation_time = wheel_interrupt_current_time - wheel_interrupt_previous_time;
+  if (wheel_rotation_time > 1) {
+    float wheel_rpm = 60.0 / (wheel_rotation_time / 1000);
+    wheel_kmph = (wheel_rpm * 60 * WHEEL_CIRCUMFORANCE) / 1000;
+  }
+
+  // crank interrupt within maximum delay
   current_loop_time = millis();
-  if(current_loop_time - last_crank_pass <= crank_pass_maximum_delay && kmph <= wheel_maximum_speed){
-    //Record first activation time
-    if(esc_activation_time < startup_time){
-      Serial.print("ESC First Activation: ");
-      Serial.println(current_loop_time);
-      esc_activation_time = current_loop_time;
-      timer_activation_count = wheel_rotations_count;
-    }
-    //Power Up ESC
+  if (current_loop_time - crank_interrupt_current_time <= CRANK_PASS_MAXIMUM_DELAY && wheel_kmph <= WHEEL_MAXIMUM_SPEED) {
+    // power up ESC
     digitalWrite(MOTOR_ACTIVITY_LED_PIN, HIGH);
-    if(esc_power_output <= esc_power_limit && (esc_power_output + esc_step_up) <= esc_power_limit){
-      Serial.print("ESC Power Output (up): ");
-      Serial.println(esc_power_output);
-      esc.write(esc_power_output);
+    if ((esc_power_output + esc_step_up) <= esc_power_limit) {
       esc_power_output += esc_step_up;
-    }else{
-      // Serial.print("ESC Power Output (Limited): ");
-      // Serial.println(esc_power_limit);
-      esc.write(esc_power_limit);
+    } else {
+      esc_power_output = esc_power_limit;
     }
-  }else{
-    //Power Down ESC
+    esc.write((int)esc_power_output);
+  } else {
+    // power down ESC
     digitalWrite(MOTOR_ACTIVITY_LED_PIN, LOW);
-    if(esc_power_output - esc_step_down > esc_initial_power){
+    if (esc_power_output - esc_step_down >= esc_initial_power) {
       esc_power_output -= esc_step_down;
-      Serial.print("ESC Power Output (down): ");
-      Serial.println(esc_power_output);
-      esc.write(esc_power_output);
-    }else{
+      esc.write((int)esc_power_output);
+    } else {
       esc_power_output = esc_initial_power;
-      // Serial.println("ESC Power STOP");
       esc.write(0);
     }
   }
-  //Output wheel rotation
-  if(esc_activation_time > startup_time){
-    if(esc_activation_time + wheel_rotation_time <= current_loop_time){
-      if(timer_deactivation_count < timer_activation_count){
-        timer_deactivation_count = wheel_rotations_count;
-        Serial.print("Total Wheel Rotations within Time: ");
-        Serial.println(timer_deactivation_count - timer_activation_count);
-      }
-    }else{
-      Serial.print("Current Wheel Rotations: ");
-      Serial.print(wheel_rotations_count);
-      Serial.print(" (");
-      Serial.print(((esc_activation_time + wheel_rotation_time) - current_loop_time)/1000);
-      Serial.println("s) left");
-    }
+
+  // display message
+  if (current_loop_time - message_previous_time >= MESSAGE_MAXIMUM_INTERVAL) {
+    message_previous_time = current_loop_time;
+    Serial.print("ESC power output: ");
+    Serial.print(esc_power_output);
+    Serial.print(" crank count: ");
+    Serial.print(crank_rotations_counter);
+    Serial.print(" wheel count: ");
+    Serial.print(wheel_rotations_counter);
+    Serial.print(" crank kmph: ");
+    Serial.print(crank_kmph);
+    Serial.print(" wheel kmph: ");
+    Serial.println(wheel_kmph);
   }
-  //Pause the system for 0.25s
-  delay(250);
+
+  // pause the system for 0.025s
+  delay(25);
 }
 
-//Crank sensor interrupt
-void crank(){
-  last_crank_pass = millis();
+/*
+  Crank Sensor Interrupt
+  ---------------------
+*/
+void crankInterrupt() {
+  crank_rotations_counter = crank_rotations_counter + 1;
+  crank_interrupt_previous_time = crank_interrupt_current_time;
+  crank_interrupt_current_time = millis();
 }
 
-//Wheel RPM sensor interrupt
-void rpm(){
-  wheel_rotations_count = wheel_rotations_count + 1;
-  last_rpm_pass_2 = last_rpm_pass;
-  last_rpm_pass = millis();
+/*
+  Wheel Sensor Interrupt
+  ---------------------
+*/
+void wheelInterrupt() {
+  wheel_rotations_counter = wheel_rotations_counter + 1;
+  wheel_interrupt_previous_time = wheel_interrupt_current_time;
+  wheel_interrupt_current_time = millis();
 }
-
